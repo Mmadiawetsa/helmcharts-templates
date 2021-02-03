0984030e5739acef80d06987d939a503c005d71c
mmadiawetsa <mmadiawetsa@yahoo.com>
2021-02-03 07:57:09 +0200

    drbug

---
+++ strapi/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '94b0b99d015b65bc23d8382001e1a483'),
+    },
+  },
+});
---
+++ strapi/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dc0807e2-b746-4d9a-a15e-9604ccb55564'
+};
